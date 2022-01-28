872076be53b8a60cdf5d6ec57801b3961710ccfc
Furkan Demir <frndemir@gmail.com>
2022-01-28 20:09:41 +0300

    :tada: beginnnnnnnnnrfkproject

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    {{name}} is mounted!
+  </main>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+main {
+  border: 2px dashed #1864ab;
+  height: 100%;
+}
+</style>
